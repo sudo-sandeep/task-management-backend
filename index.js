cd06703482d31b3a62e546d45e882bd794fbf437
Sandeep <notsandeepkumar@gmail.com>
2024-04-14 00:17:15 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-import express from 'express'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
 
+//Initializing the app
+const app = express();
 
-const app = express()
+//Middlewares
+app.use(cors());
+app.use(express.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
-app.listen(process.env.PORT || 80,'0.0.0.0')
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log("Connected to database!")
+  app.listen(process.env.PORT || 80, "0.0.0.0", () => {
+    console.log(`Listenting at http://localhost:${process.env.PORT || 80}`);
+  });
+});
